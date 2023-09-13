305ad7e2646462a46adbe4e218f874b599079386
Rasolomanana Herimanitra Olivier <herimanitraolivierr@gmail.com>
2023-09-13 08:59:56 +0300

    feat: add save and delete banner

--- app/Repositories/Events/EventRepository.ts
+++ app/Repositories/Events/EventRepository.ts
@@ -1,12 +1,44 @@
 import NotFoundException from 'App/Exceptions/NotFoundException'
 import Event from 'App/Models/Event'
+import type { MultipartFileContract } from '@ioc:Adonis/Core/BodyParser'
 import { CreateEventPayload, UpdateEventPayload } from './event.repository'
+import EventType from 'App/Models/EventType'
+import Drive from '@ioc:Adonis/Core/Drive'
 
 export default class EventRepository {
   public static async create(payload: CreateEventPayload) {
-    const event = await Event.create(payload)
+    const eventType = await EventType.find(payload.typeId)
+    if (!eventType) {
+      throw new NotFoundException(`Event with id ${payload.typeId} not found`)
+    }
+
+    let bannerPath: string | undefined
+    if (payload.banner) {
+      bannerPath = await EventRepository.saveBanner(payload.banner)
+    }
+
+    const event = await Event.create({
+      ...payload,
+      banner: bannerPath,
+    })
 
     return event
+  }
+
+  public static async saveBanner(banner: MultipartFileContract) {
+    await banner.moveToDisk('banners')
+
+    return await Drive.getUrl(`banners/${banner.fileName!}`)
+  }
+
+  public static async removeBanner(eventId: number) {
+    const event = await Event.find(eventId)
+
+    if (!event) throw new NotFoundException(`Event with ID ${eventId} not found`)
+
+    if (event.banner) {
+      await Drive.delete(event.banner)
+    }
   }
 
   public static async find(id: number) {
@@ -26,7 +58,25 @@ export default class EventRepository {
 
     if (!event) throw new NotFoundException(`Event with ID ${id} not found`)
 
-    event.merge(payload)
+    if (payload.typeId) {
+      const eventType = await EventType.find(payload.typeId)
+      if (!eventType)
+        throw new NotFoundException(`Event type with id ${payload.typeId} could not be found`)
+    }
+
+    /**
+     * Update the banner if it exists
+     */
+    let bannerPath: string | undefined
+    if (payload.banner) {
+      await EventRepository.removeBanner(id)
+      bannerPath = await EventRepository.saveBanner(payload.banner)
+    }
+
+    event.merge({
+      ...payload,
+      banner: bannerPath,
+    })
     await event.save()
 
     return event
