229655e6bf70a3cd3e20b0867e56ec2ee32c1d71
Rasolomanana Herimanitra Olivier <herimanitraolivierr@gmail.com>
2023-09-22 07:34:22 +0300

    feat: feedbacks routes

--- start/routes/v1/events.ts
+++ start/routes/v1/events.ts
@@ -12,6 +12,14 @@ Route.group(() => {
   Route.get('/events/:eventId/sessions/:sessionId', 'SessionsController.show')
 
   /**
+   * Feedbacks routes
+   *
+   * HTTP VERBS: GET - No auth required
+   */
+  Route.get('/events/:eventId/feedbacks', 'FeedbacksController.index')
+  Route.get('/events/:eventId/feedbacks/:feedbackId', 'FeedbacksController.show')
+
+  /**
    * List of the related events
    *
    * endpoint: GET /api/v1/events/:id/related
@@ -62,6 +70,15 @@ Route.group(() => {
     Route.post('/events/:eventId/sessions/:sessionId/speakers', 'SessionsController.addSpeakers')
     Route.put('/events/:eventId/sessions/:sessionId/speakers', 'SessionsController.syncSpeakers')
     Route.delete('/events/:eventId/sessions/:sessionId', 'SessionsController.delete')
+
+    /**
+     * Feedbacks routes
+     *
+     * HTTP VERBS: POST | DELETE | PUT - Auth required
+     */
+    Route.post('/events/:eventId/feedbacks', 'FeedbacksController.create')
+    Route.put('/events/:eventId/feedbacks/:feedbackId', 'FeedbacksController.update')
+    Route.delete('/events/:eventId/feedbacks/:feedbackId', 'FeedbacksController.delete')
   }).middleware('auth')
 })
   .prefix('api/v1')
